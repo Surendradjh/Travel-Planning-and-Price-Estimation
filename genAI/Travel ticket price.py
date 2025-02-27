098e1edff28881145473edb8aafc85dac2744133
surendra dj h <98631400+Surendradjh@users.noreply.github.com>
2025-02-27 15:33:56 +0530

    Update Travel ticket price.py

--- genAI/Travel ticket price.py
+++ genAI/Travel ticket price.py
@@ -5,10 +5,6 @@ st.title("🛣️ Ticket Price Estimation")
 st.subheader("Get an approximate cost for your journey")
 from langchain_core.prompts import ChatPromptTemplate
 
-# templet=ChatPromptTemplate(messages=[
-#     ('system', 'You are an AI assistant for estimating average ticket price or cost.'),
-#     ('human', 'Estimate and return the all types of classes average cost of traveling from {source} to {destination} using only {mode}.return only estimated avg costs of all classes.')])
-
 templet=ChatPromptTemplate(messages=[
     ('system', 'You are an AI assistant for estimating average ticket price or cost and plan the trip.'),
     ('human', """Estimate and return the average cost breakdown for all travel classes from {source} to {destination} using only {mode} on this date {date}. Include the estimated average cost for each class and the overall estimated average cost of all classes. Format the response as follows:\n\nHere\'s an estimated average cost breakdown for different classes:
@@ -20,8 +16,10 @@ templet=ChatPromptTemplate(messages=[
 
 #  Logic - 2 
 from langchain_google_genai import ChatGoogleGenerativeAI
-with open(r"api_key.txt",'r') as key:
-    api_key=key.readline()
+# with open(r"api_key.txt",'r') as key:
+#     api_key=key.readline()
+
+api_key="AIzaSyC9j5KaPVcanw9nvPAfKfORBqsCzBjx37I"
 chat_model=ChatGoogleGenerativeAI(api_key=api_key,model="gemini-2.0-flash-exp")
 
 
